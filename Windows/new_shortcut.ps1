8289d2de2f6cc51742071aa22ececb22ad86493c
Connor Bowyer <81127870+TheReywob@users.noreply.github.com>
2022-02-17 18:51:04 -0500

    Add Powershell shortcut script

---
+++ Windows/new_shortcut.ps1
@@ -0,0 +1,22 @@
+# ===========================
+#  Author: Connor Bowyer
+#  Date: 2/17/2022
+#  Description:
+#
+#  This script will create a
+# shortcut with a custom
+# loaded icon.
+# ===========================
+$userInstallLocation = $args[0]
+$shortcutPath = "C:\Users\$env:UserName\Desktop\Binance Trade Bot.lnk"
+$iconPath = "$userInstallLocation\binance-trade-bot\start_scripts\robot_icon.ico"
+$targetPath = "$userInstallLocation\binance-trade-bot\start_scripts\start_script_WINDOWS.bat"
+
+$Shell = New-Object -ComObject ("WScript.Shell")
+#Create shortcut
+$Shortcut = $Shell.CreateShortcut($shortcutPath)
+# Set the target path to link the shortcut to
+$Shortcut.TargetPath = "$targetPath"
+# Change the icon of the shortcut
+$Shortcut.IconLocation = "$iconPath"
+$Shortcut.Save()
